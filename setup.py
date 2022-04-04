a50b5ea9ec821765e68e061e8d3766b8df0d1040
jonvet <jonas@moonfire.com>
2022-04-04 19:40:47 +0100

    Install requirements in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from distutils.core import setup
 
+with open("requirements.txt") as f:
+    REQUIREMENTS = f.read().splitlines()
+
 setup(
     name="moonfire_tokenomics",
     version="1.0",
+    install_requires=REQUIREMENTS,
     extras_require={
         "testing": ["pytest"],
     },
